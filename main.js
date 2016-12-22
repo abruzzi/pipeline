a813ee4a34da7ae95b7e5f8859201fa3df434443
Qiu Juntao <juntao.qiu@gmail.com>
2016-12-22 19:34:47 +1100

    simple implementation for the first version

--- main.js
+++ main.js
@@ -15,133 +15,70 @@ $(function() {
 
     var editor = new JSONEditor(container, options);
     
-    $.get('/data/pipeline.json').done(function(data) {
+    $.get('/data/pipeline-1.json').done(function(data) {
         editor.set(data);
+        draw(data, false);
+    });
+
+  // Set up zoom support
+  var svg = d3.select("svg"),
+      inner = svg.select("g"),
+      zoom = d3.behavior.zoom().on("zoom", function() {
+        inner.attr("transform", "translate(" + d3.event.translate + ")" +
+                                    "scale(" + d3.event.scale + ")");
+      });
+
+  svg.call(zoom);
+  var render = new dagreD3.render();
+  
+  // Left-to-right layout
+  var g = new dagreD3.graphlib.Graph();
+
+  g.setGraph({
+    nodesep: 70,
+    ranksep: 50,
+    rankdir: "LR",
+    marginx: 20,
+    marginy: 20
+  });
+
+  function draw(data, isUpdate) {
+    _.each(data.states, function(state, id) {
+        var className = state.name ? "running" : "stopped";
+        var html = "<div>";
+          html += "<span class='status'></span>";
+          html += "<span class='consumers'>"+state.name+"</span>";
+          html += "</div>";
+      
+      g.setNode(id, {
+        labelType: "html",
+        label: html,
+        rx: 5,
+        ry: 5,
+        padding: 0,
+        class: className
+      });
+
+      var x = _.keys(data.flow[state.name])[0];
+      if(x) {
+          g.setEdge(id, x, {
+              label: "edge",
+              width: 40
+            });  
+      }
     })
 
-    var graph = new joint.dia.Graph;
-
-    var paper = new joint.dia.Paper({
-
-        el: document.getElementById('paper'),
-        width: 800,
-        height: 400,
-        gridSize: 1,
-        model: graph,
-        snapLinks: true,
-        linkPinning: false,
-        embeddingMode: true,
-        highlighting: {
-            'default': {
-                name: 'stroke',
-                options: {
-                    padding: 6
-                }
-            },
-            'embedding': {
-                name: 'addClass',
-                options: {
-                    className: 'highlighted-parent'
-                }
-            }
-        },
-
-        validateEmbedding: function(childView, parentView) {
-
-            return parentView.model instanceof joint.shapes.devs.Coupled;
-        },
-
-        validateConnection: function(sourceView, sourceMagnet, targetView, targetMagnet) {
-
-            return sourceMagnet != targetMagnet;
-        }
-    });
-
-    var connect = function(source, sourcePort, target, targetPort) {
-
-        var link = new joint.shapes.devs.Link({
-            source: {
-                id: source.id,
-                port: sourcePort
-            },
-            target: {
-                id: target.id,
-                port: targetPort
-            }
-        });
-
-        link.addTo(graph).reparent();
-    };
-
-    var c1 = new joint.shapes.devs.Coupled({
-
-        position: {
-            x: 230,
-            y: 50
-        },
-        size: {
-            width: 300,
-            height: 300
-        }
-    });
-
-    c1.set('inPorts', ['in']);
-    c1.set('outPorts', ['out 1', 'out 2']);
-
-    var a1 = new joint.shapes.devs.Atomic({
-
-        position: {
-            x: 360,
-            y: 260
-        },
-        inPorts: ['xy'],
-        outPorts: ['x', 'y']
-    });
-
-    var a2 = new joint.shapes.devs.Atomic({
-
-        position: {
-            x: 50,
-            y: 160
-        },
-        outPorts: ['out']
-    });
-
-    var a3 = new joint.shapes.devs.Atomic({
-
-        position: {
-            x: 650,
-            y: 50
-        },
-        size: {
-            width: 100,
-            height: 300
-        },
-        inPorts: ['a', 'b']
-    });
-
-    graph.addCells([c1, a1, a2, a3]);
-
-    c1.embed(a1);
-
-    connect(a2, 'out', c1, 'in');
-    connect(c1, 'in', a1, 'xy');
-    connect(a1, 'x', c1, 'out 1');
-    connect(a1, 'y', c1, 'out 2');
-    connect(c1, 'out 1', a3, 'a');
-    connect(c1, 'out 2', a3, 'b');
-
-    /* rounded corners */
-
-    _.each([c1, a1, a2, a3], function(element) {
-
-        element.attr({
-            '.body': {
-                'rx': 6,
-                'ry': 6
-            }
-        });
-    });
-
+    inner.call(render, g);
+    // Zoom and scale to fit
+    var graphWidth = g.graph().width + 80;
+    var graphHeight = g.graph().height + 40;
+    var width = parseInt(svg.style("width").replace(/px/, ""));
+    var height = parseInt(svg.style("height").replace(/px/, ""));
+    var zoomScale = Math.min(width / graphWidth, height / graphHeight);
+    var translate = [(width/2) - ((graphWidth*zoomScale)/2), (height/2) - ((graphHeight*zoomScale)/2)];
+    zoom.translate(translate);
+    zoom.scale(zoomScale);
+    zoom.event(isUpdate ? svg.transition().duration(500) : d3.select("svg"));
+  }
 });
 
